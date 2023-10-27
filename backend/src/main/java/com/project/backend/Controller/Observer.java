702835e3e8056150ccce0f7c871f01a28735077f
Rajdeep Basak <rajdeepbasak2000@gmail.com>
2023-10-27 20:26:35 +0530

    Last And final Commit

--- backend/src/main/java/com/project/backend/Controller/Observer.java
+++ backend/src/main/java/com/project/backend/Controller/Observer.java
@@ -1,4 +1,5 @@
 package com.project.backend.Controller;
+
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.text.ParseException;
@@ -23,22 +24,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 @RestController
 @CrossOrigin("*")
 public class Observer {
 	@Autowired
 	Servo samp;
-	@PostMapping(value = "/user/save",consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces =MediaType.APPLICATION_JSON_VALUE)
+
+	@PostMapping(value = "/user/save", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	@Async
 	public void save(@RequestPart("dob") String dob, @RequestPart("email") String email,
 			@RequestPart("mobileno") String phno, @RequestPart("username") String username,
 			@RequestPart("password") String password, @RequestPart("type") String type,
-			@RequestPart("deg") String desx, @RequestPart("about") String about,@RequestPart("address") String address,
-			@RequestPart(name="fb",required = false) String fb,@RequestPart(name="insta",required = false) String insta,@RequestPart(name="twitter" ,required = false) String twitter, @RequestPart("name") String name,
-			@RequestParam(name="file",required = false) MultipartFile file) {
+			@RequestPart("deg") String desx, @RequestPart("about") String about, @RequestPart("address") String address,
+			@RequestPart(name = "fb", required = false) String fb,
+			@RequestPart(name = "insta", required = false) String insta,
+			@RequestPart(name = "twitter", required = false) String twitter, @RequestPart("name") String name,
+			@RequestParam(name = "file") MultipartFile file) {
 		Users data = new Users();
 		try {
-			System.out.print(dob+username+password+type+desx+about+name+file.getSize());
+			System.out.print(dob + username + password + type + desx + about + name + file.getSize());
 			data.setCreatedat(new Timestamp(System.currentTimeMillis()));
 			data.setUpdatedat(new Timestamp(System.currentTimeMillis()));
 			data.setDob(new SimpleDateFormat("yyyy-MM-dd").parse(dob));
@@ -59,50 +64,59 @@ public class Observer {
 			e.printStackTrace();
 		}
 	}
+
 	@GetMapping(value = "/user/getall")
 	public List<Users> name() {
 		return samp.getAll();
 	}
+
 	@Async
-	@GetMapping(value="/user/get/{username}")
+	@GetMapping(value = "/user/get/{username}")
 	public Users getuser(@PathVariable("username") String username) {
 		return samp.getusername(username);
 	}
+
 	@DeleteMapping(value = "/user/delete/{id}")
 	public void dell(@RequestParam("id") String id) {
 		samp.deleteUser(Integer.parseInt(id));
 	}
+
 	@PutMapping(value = "/user/update/{userName}")
 	@Async
-	public void UpdateUser(@RequestPart(name="dob",required = false) String dob, @RequestPart(name="email",required = false) String email,
-			@RequestPart(name="phno",required = false) String phno, @PathVariable(name="userName") String username,
-			@RequestPart(name="password",required = false) String password,@RequestPart(name="id",required = false) Integer id,
-			@RequestPart(name="desx",required = false) String desx, @RequestPart(name="about",required = false) String about,@RequestPart("address") String address ,@RequestPart(name="name",required = false) String name) {
+	public void UpdateUser(@RequestPart(name = "dob", required = false) String dob,
+			@RequestPart(name = "email", required = false) String email,
+			@RequestPart(name = "phno", required = false) String phno, @PathVariable(name = "userName") String username,
+			@RequestPart(name = "password", required = false) String password,
+			@RequestPart(name = "id", required = false) Integer id,
+			@RequestPart(name = "desx", required = false) String desx,
+			@RequestPart(name = "about", required = false) String about, @RequestPart("address") String address,
+			@RequestPart(name = "name", required = false) String name) {
 		Users data = samp.getUsers(username);
 		try {
 			data.setUpdatedat(new Timestamp(System.currentTimeMillis()));
-			if(dob!=null)
+			if (dob != null)
 				data.setDob(new SimpleDateFormat("yyyy-MM-dd").parse(dob));
-			if(email!=null)
+			if (email != null)
 				data.setEmail(email);
-			if(address != null)
+			if (address != null)
 				data.setAddress(address);
-			if(phno!=null)
+			if (phno != null)
 				data.setPhno(phno);
 			data.setUserName(username);
-			if(password!=null)
+			if (password != null)
 				data.setPassword(password);
-			if(about!=null)
+			if (about != null)
 				data.setAbout(about);
-			if(desx!=null)
+			if (desx != null)
 				data.setDesx(desx);
-			if(name!=null)
+			if (name != null)
 				data.setName(name);
 			samp.savex(data);
 		} catch (ParseException | IOException e) {
 			e.printStackTrace();
 		}
 	}
+
 	@Async
 	@GetMapping("/user/get/file/{id}")
 	public ResponseEntity<byte[]> getFile(@PathVariable Integer id) {
